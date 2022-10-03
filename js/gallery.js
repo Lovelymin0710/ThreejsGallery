9675acd7723ac78edfaf3cd472d4a1c77499efb9
Hyjiii <wjsguswl310@naver.com>
2022-10-03 14:12:32 +0900

    organize code

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,5 @@
 import * as THREE from "https://unpkg.com/three@0.108.0/build/three.module.js";
 import { OrbitControls } from "https://unpkg.com/three@0.108.0/examples/jsm/controls/OrbitControls.js";
-import { TrackballControls } from "https://unpkg.com/three@0.108.0/examples/jsm/controls/TrackballControls.js";
-// import Stats from 'https://unpkg.com/three@0.108.0/examples/jsm/libs/stats.module'
-// import { KeyController } from "./keyController.js";
-import {PointerLockControls} from "https://unpkg.com/three@0.108.0/examples/jsm/controls/PointerLockControls.js";
 import { FontLoader } from "../loaders/FontLoader.js";
 import { TextGeometry } from '../geometries/TextGeometry.js';
 
@@ -12,7 +8,7 @@ let HEIGHT = window.innerHeight;
 let wallWidth;
 
 // const totalNum = 10; //전체 액자 갯수
-const distance = 110; //액자 사이 거리
+const distance = 130; //액자 사이 거리
 
 let totalNum;
 let scene, camera, renderer, controls;
@@ -23,49 +19,57 @@ const workArr = [
     {
         "image": "https://source.unsplash.com/collection/0",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/1",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/2",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/3",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/4",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/5",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/6",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     },
     {
         "image": "https://source.unsplash.com/collection/7",
         "link": "http://google.com",
+        "info" : '저자1 신기'
     }
 ];
 
 const init = () => {
-    
     totalNum = workArr.length - 1; //전체 박스 갯수
 
     scene = new THREE.Scene();
 
     scene.background = new THREE.Color("#99B1F0"); //배경 컬러
-    camera = new THREE.PerspectiveCamera(30, WIDTH / HEIGHT, 0.1, 1000);
-    camera.position.set(220, 50, 220);
+    camera = new THREE.PerspectiveCamera(30, WIDTH / HEIGHT, 10, 2000);
+    camera.position.set(300, 50, 300);
 
     renderer = new THREE.WebGLRenderer({ antialias: true});
     renderer.setSize(WIDTH, HEIGHT);
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFShadowMap; //PCFShadowMap
     //그림자 활성화
@@ -81,11 +85,11 @@ const init = () => {
 
     //조명 넣기
     var light = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFF, 0.45);
-    light.position.set(0, 70, -50);
+    light.position.set(0, 80, -50);
     scene.add(light);
 
     var light2 = new THREE.HemisphereLight(0xFFFFFF, 0xFFFFFF, 0.45);
-    light2.position.set(-50, 70, 0);
+    light2.position.set(-50, 80, 0);
     scene.add(light2);
 
     const helper = new THREE.HemisphereLightHelper( light, 5 );
@@ -93,9 +97,10 @@ const init = () => {
     const helper2 = new THREE.HemisphereLightHelper( light2, 5 );
     scene.add( helper2 );
 
-
     controls = new OrbitControls(camera, renderer.domElement);
-    controls.keyPanSpeed=15;
+    controls.maxDistance=1000;
+    // controls.maxPolarAngle = Math.PI / 3; // 수직 궤도
+    controls.keyPanSpeed=20;
 
     {
         //가벽 만들기
@@ -106,26 +111,25 @@ const init = () => {
         imageMap.repeat.set(10, 4);
 
         wallWidth = distance * totalNum + distance;
-        const geometry = new THREE.BoxGeometry(wallWidth, 100, 2); //x,y,z(두께)
-        const material = new THREE.MeshPhongMaterial({
-            // map: imageMap,
-            color: 0xF4FAB1,
-        });
+        const geometry = new THREE.BoxGeometry(wallWidth, 150, 2); //x,y,z(두께)
+        const material = new THREE.MeshPhongMaterial({color: 0xF4FAB1});
         const wallMesh = new THREE.Mesh(geometry, material);
         wallMesh.position.set(0, 0, -wallWidth/2);
         //액자 시작 x축 원점(제어유용), 두께가 2니까 뒤로 좀 빼줌
         wallMesh.receiveShadow = true; //그림자 표시
         // wallMesh.castShadow = true; //그림자 발생
+        wallMesh.frustumCulled=false;
         galleryGroup.add(wallMesh);
         scene.add(galleryGroup);
 
-        const geometry2 = new THREE.BoxGeometry(2, 100, wallWidth);
+        const geometry2 = new THREE.BoxGeometry(2, 150, wallWidth);
+        geometry2.computeBoundingSphere();
         const wallMesh2 = new THREE.Mesh(geometry2, material);
-    
         wallMesh2.position.set(-wallWidth/2, 0, 0);
         //액자 시작 x축 원점(제어유용), 두께가 2니까 뒤로 좀 빼줌
         wallMesh2.receiveShadow = true; //그림자 표시
         // wallMesh.castShadow = true; //그림자 발생
+        wallMesh.frustumCulled=false;
         galleryGroup2.add(wallMesh2);
         scene.add(galleryGroup2);
     }
@@ -138,8 +142,9 @@ const init = () => {
         new THREE.BoxGeometry(wallWidth, 0.1, wallWidth), 
         new THREE.MeshPhongMaterial({color: 0xFFFFFF})
     );
+    floor.frustumCulled=false;
+    floor.position.set(0, -75, 0);
     scene.add(floor);
-    floor.position.set(0, -50, 0);
     // floor.receiveShadow=true;
 
     //텍스트 로딩
@@ -156,9 +161,11 @@ const init = () => {
         const material = new THREE.MeshBasicMaterial({color: 0xffffff});
         const font3d = new THREE.Mesh(geometry, material);
         // font3d.position.set(-wallWidth/4+84,-50, -wallWidth/4)
-        font3d.position.set(-200,-50, 50)
+        font3d.position.set(-225,-70, 20)
         // font3d.rotateZ(180)
-        font3d.rotateY(44.7)
+        font3d.rotateY(44.8)
+        font3d.castShadow=true;
+        font3d.receiveShadow=true;
         scene.add(font3d);
     })
 };
@@ -170,33 +177,34 @@ const addBox = (i) => {
     // );
     const imageMap = new THREE.TextureLoader().load(workArr[i].image);
     console.log(imageMap);
-    const geometry = new THREE.BoxGeometry(40, 28, 1);
+    const geometry = new THREE.BoxGeometry(52, 35, 2);
     const material = new THREE.MeshPhongMaterial({map: imageMap});
     const boxMesh = new THREE.Mesh(geometry, material);
     boxMesh.castShadow = true;
     let x = -(wallWidth/2)+ (i+1) * distance;
     // let x = (wallWidth/workArr.length)*i
     console.log(x);
-    let y = 0; //Math.random() * 40 - 5;
+    let y = 10; //Math.random() * 40 - 5;
     let z = 0;
     boxMesh.position.set(x, y, -wallWidth/2+2);
     boxMesh.name = 'workofArt_${i}';
     boxMesh.link = workArr[i].link;
+    boxMesh.info = workArr[i].info;
     galleryGroup.add(boxMesh);
 
-    const geometry2 = new THREE.BoxGeometry(1, 28, 40);
+    const geometry2 = new THREE.BoxGeometry(2, 35, 52);
     const boxMesh2 = new THREE.Mesh(geometry2, material);
     boxMesh2.castShadow = true;
     boxMesh2.position.set(-wallWidth/2+2, y, x);
     boxMesh2.name = 'workofArt_${i}';
     boxMesh2.link = workArr[i].link;
+    boxMesh2.info = workArr[i].info;
     galleryGroup2.add(boxMesh2);
 
     //조명 넣기
- 
     const spotLight = new THREE.SpotLight(0xffffff, 1);
-    spotLight.position.set(x, 34, 12-wallWidth/2+20);
-    spotLight.angle = Math.PI / 5; //조명 범위
+    spotLight.position.set(x, 45, 12-wallWidth/2+20);
+    spotLight.angle = Math.PI / 4; //조명 범위
     spotLight.penumbra = 0.1; //조명 경계 정도
     spotLight.decay = 1.2; //조명 투명도
     spotLight.distance = 70;
@@ -209,8 +217,8 @@ const addBox = (i) => {
     // scene.add(spotLightHelper);
 
     const spotLight2 = new THREE.SpotLight(0xffffff, 1);
-    spotLight2.position.set(12-wallWidth/2+20, 34, x);
-    spotLight2.angle = Math.PI / 5; //조명 범위
+    spotLight2.position.set(12-wallWidth/2+20, 45, x);
+    spotLight2.angle = Math.PI / 4; //조명 범위
     spotLight2.penumbra = 0.1; //조명 경계 정도
     spotLight2.decay = 1.2; //조명 투명도
     spotLight2.distance = 70;
@@ -221,6 +229,7 @@ const addBox = (i) => {
 
     // const spotLightHelper2 = new THREE.SpotLightHelper(spotLight2);
     // scene.add(spotLightHelper2);
+    
 };
 
 const raycaster = new THREE.Raycaster();
@@ -242,17 +251,18 @@ const onPointerMove = (event) => {
     // }
 
     if (intersects.length > 0 || intersects2.length > 0) {
-        if (intersects[1] != null || intersects2[1] != null){
+        if (intersects[1] != null){
             document.querySelector("body").style.cursor = "pointer";
-            console.log(intersects2)
+            console.log(intersects[0].object.info)
+        }
+        else if(intersects2[1] != null){
+            document.querySelector("body").style.cursor = "pointer";
         }
         else{
-            document.querySelector("body").style.cursor = "auto"; 
+            document.querySelector("body").style.cursor = "auto";
         }
     } else {
         document.querySelector("body").style.cursor = "auto"; 
-        // console.log(galleryGroup.children)
-        //닿았을 때만 변경 아니면 다시 돌아옴, 빼면 바뀐상태 유지
     }//마우스 닿으면 커서 포인터로 바뀜(바디 태그 변경)
 };
 
@@ -285,11 +295,11 @@ const stageResize = () => {
     HEIGHT = window.innerHeight;
 
     camera.updateProjectionMatrix();
-    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(WIDTH, HEIGHT);
     camera.aspect = WIDTH / HEIGHT;
 };
 const animate = () => {
+    controls.update();
     // camera.lookAt(scene.position);
     camera.updateProjectionMatrix();
     renderer.render(scene, camera);
